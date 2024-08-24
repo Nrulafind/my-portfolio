54b0c68b487dc348f376a177073f84bba454396a
Nrulafind <mnurula.nu246@gmail.com>
2024-08-24 23:26:02 +0700

    Updates

---
+++ assets/script.js
@@ -0,0 +1,84 @@
+document.addEventListener("DOMContentLoaded", function () {
+    let elements = document.getElementsByClassName('typewrite');
+    for (let i = 0; i < elements.length; i++) {
+        let toRotate = elements[i].getAttribute('data-type');
+        let period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
+        }
+    }
+});
+
+let TxtType = function (el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
+};
+
+TxtType.prototype.tick = function () {
+    let i = this.loopNum % this.toRotate.length;
+    let fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
+        this.txt = fullTxt.substring(0, this.txt.length - 1);
+    } else {
+        this.txt = fullTxt.substring(0, this.txt.length + 1);
+    }
+
+    this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
+
+    let that = this;
+    let delta = 200 - Math.random() * 100;
+
+    if (this.isDeleting) {
+        delta /= 2;
+    }
+
+    if (!this.isDeleting && this.txt === fullTxt) {
+        delta = this.period;
+        this.isDeleting = true;
+    } else if (this.isDeleting && this.txt === '') {
+        this.isDeleting = false;
+        this.loopNum++;
+        delta = 500;
+    }
+
+    setTimeout(function () {
+        that.tick();
+    }, delta);
+};
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const bubleHeroes = document.querySelectorAll('.buble-hero');
+    const images = [
+        './assets/img/web-development.png',
+        './assets/img/machine-learning.png',
+        './assets/img/server.png',
+        './assets/img/project-management.png',
+    ];
+
+    bubleHeroes.forEach((buble, index) => {
+        const delay = index * 1.25;
+        buble.style.animationDelay = `${delay}s`;
+        buble.style.backgroundImage = `url(${images[index]})`;
+    });
+});
+
+const style = document.createElement('style');
+style.type = 'text/css';
+style.innerHTML = `
+    @keyframes orbit {
+        0% {
+            transform: rotate(0deg) translateX(300px) rotate(0deg);
+        }
+        100% {
+            transform: rotate(360deg) translateX(300px) rotate(-360deg);
+        }
+    }
+`;
+document.head.appendChild(style);
